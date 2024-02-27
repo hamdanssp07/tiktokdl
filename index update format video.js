bbfa3f99df1b652db435d0bdfeffcddab68c26f0
hamdanssp07 <110283347+hamdanssp07@users.noreply.github.com>
2024-02-27 16:53:41 +0700

    Add files via upload

---
+++ index update format video.js
@@ -0,0 +1,19 @@
+// index.js
+
+// Import tiktokdl function from media.js
+const { tiktokdl } = require('./lib/media.js');
+
+// Example TikTok video URL
+const url = 'https://www.tiktok.com/@omagadsus/video/7025456384175017243?is_from_webapp=1&sender_device=pc&web_id6982004129280116226';
+
+// Prompt user to choose a format (you can customize this part)
+const userFormat = prompt('Choose a video format (mp4/avi):');
+
+// Call tiktokdl function with the chosen format
+const data = await tiktokdl(url, userFormat);
+
+// Log the data (you can modify this part as needed)
+console.log(data);
+
+
+
